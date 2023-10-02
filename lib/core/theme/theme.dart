4a127249ef81e08543cb249d0801449ee585eb49
Ganesh Bhat <ganeshbhat015@gmail.com>
2023-10-02 17:03:02 +0530

    Update appbar theme

--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 final ThemeData lightThemeData = ThemeData(
-    useMaterial3: true,
-    cardColor: Colors.deepPurpleAccent.withOpacity(0.1),
-    colorSchemeSeed: Colors.deepPurpleAccent,
-    appBarTheme: const AppBarTheme(color: Colors.deepPurpleAccent));
+  useMaterial3: true,
+  cardColor: Colors.deepPurpleAccent.withOpacity(0.1),
+  colorSchemeSeed: Colors.deepPurpleAccent,
+  appBarTheme: const AppBarTheme(
+    color: Colors.deepPurpleAccent,
+    foregroundColor: Colors.white,
+  ),
+);
