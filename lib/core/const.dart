6d4a48050ad894d6f7d6e623b47ac858f6da78ef
Swaroop Sambhayya <swaroop.sambhayya@gmail.com>
2023-10-03 00:05:51 +0530

    leader board UI

--- lib/core/const.dart
+++ lib/core/const.dart
@@ -1,1 +1,17 @@
-enum Categories { history, music, science, geography }
+import 'package:flutter/material.dart';
+
+const silverConstants = [
+  Color(0xff70706F),
+  Color(0xffB3B6B5),
+  Color(0xffBEC0C2)
+];
+
+const goldenConstants = [Color(0xffEEB609), Color(0xffFCC201), Colors.yellow];
+
+const bronzeConstants = [Color(0xff9C7A3C), Color(0xffB08D57)];
+
+List<Color> tileGradients = [
+  Colors.deepPurpleAccent,
+  Colors.deepPurpleAccent.shade200,
+  Colors.deepPurpleAccent.shade100
+];
--- lib/features/leader_board/leader_board.dart
+++ lib/features/leader_board/leader_board.dart
@@ -1,10 +1,108 @@
 import 'package:flutter/material.dart';
+import 'package:quiz/core/const.dart';
 
 class LeaderBoard extends StatelessWidget {
   const LeaderBoard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: const Text("Leader Board"),
+            stretch: true,
+            expandedHeight: 225,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Padding(
+                padding: const EdgeInsets.all(25.0).copyWith(bottom: 10),
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    RankDP(
+                      gradients: silverConstants,
+                      onlineId: 'swaroop',
+                    ),
+                    RankDP(gradients: goldenConstants, onlineId: 'ganesh'),
+                    RankDP(gradients: bronzeConstants, onlineId: 'ramesh')
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SliverList.builder(
+            itemBuilder: (context, index) => ListTile(
+              leading: RankDP(
+                gradients: tileGradients,
+                onlineId: "swaroop",
+                tileMode: true,
+              ),
+              title: const Text('Swaroop S'),
+              subtitle: const Text('#swaroop'),
+              trailing: const Text('400pt'),
+            ),
+            itemCount: 10,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class RankDP extends StatelessWidget {
+  final List<Color> gradients;
+  final String onlineId;
+  final bool tileMode;
+  const RankDP(
+      {super.key,
+      required this.gradients,
+      required this.onlineId,
+      this.tileMode = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        if (!tileMode)
+          Text(
+            gradients == goldenConstants
+                ? "1"
+                : gradients == silverConstants
+                    ? "2"
+                    : "3",
+            style: const TextStyle(
+                fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+        Container(
+          padding:
+              tileMode ? const EdgeInsets.all(2.5) : const EdgeInsets.all(5),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            gradient: LinearGradient(colors: gradients),
+          ),
+          child: CircleAvatar(
+            radius: tileMode
+                ? 20
+                : gradients == goldenConstants
+                    ? 45
+                    : 35,
+            backgroundImage: NetworkImage('https://robohash.org/$onlineId.png'),
+          ),
+        ),
+        if (!tileMode)
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Text(
+              "#$onlineId",
+              style: const TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+          )
+      ],
+    );
   }
 }
